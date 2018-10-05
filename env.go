15ebe3dcf8ccf7385c12c7aff26f08074f5b447d
orvice <orvice@gmail.com>
2018-10-05 23:41:08 +0800

    add ws env

--- env.go
+++ env.go
@@ -6,10 +6,17 @@ var (
 	netInterfaceName string
 	webAddr          string
 	grpcAddr         string
+
+	enableWS bool
 )
 
 func InitEnv() {
 	netInterfaceName = env.Get("NET_INTERFACE", "eth0")
 	webAddr = env.Get("WEB_ADDR", ":8080")
 	grpcAddr = env.Get("GRPC_ADDR", ":8090")
+
+	enableWs := env.Get("ENABLE_WS", "false")
+	if enableWs == "true" {
+		enableWS = true
+	}
 }
