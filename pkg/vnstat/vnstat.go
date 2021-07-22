6cc2814ce934e5753f9a8816a5ae294eebe68e73
orvice <orvice@gmail.com>
2021-07-22 19:02:46 +0800

    dont be panic

--- pkg/vnstat/vnstat.go
+++ pkg/vnstat/vnstat.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/orvice/monitor-client/mod"
+	"github.com/weeon/log"
 	"net"
 	"os/exec"
 )
@@ -11,11 +12,12 @@ import (
 // fork form https://github.com/stnight/go-vnstat/blob/master/functions.go
 
 // This function will execute vnstat command
-func VN(netinterface string) mod.VNResult {
-	cmd := exec.Command("vnstat", "-m", "-i", netinterface, "--json")
+func VN(netInterface string) mod.VNResult {
+	cmd := exec.Command("vnstat", "-m", "-i", netInterface, "--json")
 	stdout, err := cmd.Output()
 	if err != nil {
-		panic(err.Error())
+		log.Errorf("get vnstat ret error %s", err.Error())
+		return mod.VNResult{}
 	}
 	err = cmd.Start()
 	if err != nil {
@@ -26,7 +28,8 @@ func VN(netinterface string) mod.VNResult {
 	var vnRes mod.VNResult
 	err = json.Unmarshal(b, &vnRes)
 	if err != nil {
-		panic(err)
+		log.Errorf("json.Unmarshal error %s", err.Error())
+		return mod.VNResult{}
 	}
 	return vnRes
 }
@@ -36,7 +39,8 @@ func GetAllNetInterfaces() []mod.NetInterface {
 	var allNetInterfaces []mod.NetInterface
 	interfaces, err := net.Interfaces()
 	if err != nil {
-		panic(err.Error())
+		log.Errorf("get net interfaces error %s", err.Error())
+		return nil
 	}
 	for key := range interfaces {
 		allNetInterfaces = append(allNetInterfaces, mod.NetInterface{
